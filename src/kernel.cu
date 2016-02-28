7daa791c846352fecd2757b0fd9eb5edce9a1d82
Derek Quam <quam@cat.com>
2016-02-28 09:21:56 -0600

    Socket work and pitch and yaw.

--- src/kernel.cu
+++ src/kernel.cu
@@ -25,7 +25,7 @@ void cuInRange(const cv::gpu::PtrStepSz<uchar4>& src, cv::gpu::PtrStep<uchar> ou
                unsigned char hue_low, unsigned char sat_low, unsigned char val_low,
                unsigned char hue_high, unsigned char sat_high, unsigned char val_high)
 {
-  dim3 block(32, 8);
-  dim3 grid((src.cols + block.x - 1)/block.x, (src.rows + block.y - 1)/ block.y);
+  dim3 block(16, 16);
+  dim3 grid((src.cols + block.x - 1)/block.x, (src.rows + block.y - 1)/ block.y);
   _InRange<<<grid, block>>>(src, out, hue_low, sat_low, val_low, hue_high, sat_high, val_high);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <chrono>
+#include <iomanip>
 
 //ZED include
 #include <zed/Mat.hpp>
@@ -14,6 +15,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/gpu/gpu.hpp>
 
 // Cuda functions include
 #include "kernel.cuh"
@@ -27,6 +29,7 @@ static const float WIDTH_MM = 508.f; // mm
 static const float RATIO = WIDTH / HEIGHT;
 static const float RATIO_THRESHOLD = 0.2f * RATIO;
 static const float AREA_THRESHOLD = 3000; // px^2
+static const float AREA_THRESHOLD_TOP = 8000; // px^2
 static const float HORIZ_VIEW_ANGLE_DEG = 110.f; //degrees
 static const float HORIZ_VIEW_ANGLE = HORIZ_VIEW_ANGLE_DEG * M_PI / 180.f; //radians
 static const float VERT_VIEW_ANGLE_DEG = 110.f; //degrees
@@ -36,6 +39,13 @@ static const float VERT_VIEW_ANGLE = VERT_VIEW_ANGLE_DEG * M_PI / 180.f; //radia
 static const float TEST_H = 812.8f;
 static const float TEST_W = 101.6f;
 
+#ifdef __linux__
+#include <sys/types.h>
+#include <sys/socket.h>
+#else
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+#endif
+
 
 float calculateDistance(float real_dim, float measured_px, float focal_length_px)
 {
@@ -43,20 +53,16 @@ float calculateDistance(float real_dim, float measured_px, float focal_length_px
   return real_dim / measured_px * focal_length_px;
 }
 
-cv::Rect findRect(Mat& d_mat, cv::gpu::GpuMat& d_hsv)
+cv::Rect findRect(cv::Mat& hsv)
 {
-  cv::Mat hsv;
-  cv::gpu::GpuMat d_left_cv(d_mat.height, d_mat.width, d_mat.data_type == sl::zed::UCHAR ? CV_8UC4 : CV_32FC4, d_mat.data, d_mat.step);
-  cuInRange(d_left_cv, d_hsv, 0, 0, 80, 255, 255, 255);
-  d_hsv.download(hsv);
   std::vector<std::vector<cv::Point>> contours;
   cv::findContours(hsv, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-  float min_ratio = FLT_MAX;
+  float min_ratio = 1000.f;
   cv::Rect best_fit;
   bool found = false;
   for (auto& contour : contours) {
     cv::Rect rect = cv::boundingRect(contour);
-    if (rect.area() > AREA_THRESHOLD) {
+    if (rect.area() > AREA_THRESHOLD && rect.area() < AREA_THRESHOLD_TOP) {
       float ratio = fabs(float(rect.width) / float(rect.height) - RATIO);
       if (ratio < RATIO_THRESHOLD && ratio < min_ratio) {
         min_ratio = ratio;
@@ -69,6 +75,31 @@ cv::Rect findRect(Mat& d_mat, cv::gpu::GpuMat& d_hsv)
     return best_fit;
   }
   throw std::runtime_error("No rect found.");
+}
+
+cv::Rect findRect(const Mat& d_mat, cv::gpu::GpuMat& d_hsv, cv::gpu::GpuMat& d_thresh)
+{
+  cv::Mat hsv;
+  cv::gpu::GpuMat d_left_cv(d_mat.height, d_mat.width, d_mat.data_type == sl::zed::UCHAR ? CV_8UC4 : CV_32FC4, d_mat.data, d_mat.step);
+  cv::gpu::cvtColor(d_left_cv, d_hsv, CV_RGB2HSV, 4);
+  cuInRange(d_hsv, d_thresh, 30, 0, 160, 65, 255, 255);
+  d_thresh.download(hsv);
+  //for (int value = 255; value > 0; value -= 10) {
+  //  std::cout << value << std::endl;
+  //  cuInRange(d_hsv, d_thresh, 30, 0, 160, 65, 255, 255);
+  //  d_thresh.download(hsv);
+  //  cv::imshow("hsv", hsv);
+  //  cv::waitKey(2000);
+  //}
+  return findRect(hsv);
+}
+
+cv::Rect findRect_cpu(cv::Mat& mat)
+{
+  cv::Mat hsv;
+  cv::cvtColor(mat, hsv, CV_RGB2HSV);
+  cv::inRange(hsv, cv::Scalar(40, 0, 140), cv::Scalar(255, 255, 255), hsv);
+  return findRect(hsv);
 }
 
 float findDepthMaxConfidence(Camera* zed, const cv::Rect& rect)
@@ -89,6 +120,19 @@ float findDepthMaxConfidence(Camera* zed, const cv::Rect& rect)
   Mat depth = zed->retrieveMeasure(DEPTH);
   float* depth_ptr = (float*)depth.data;
   return depth_ptr[min_conf.y * (depth.step/sizeof(float))+min_conf.y];
+}
+
+void Send(int s, std::string message, struct sockaddr_in& si_other, int slen)
+{
+#define BUFLEN 512
+  char buf[BUFLEN];
+  //send the message
+  std::cout << "Sending " << message.c_str() << std::endl;
+  if (sendto(s, message.c_str(), message.size(), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
+  {
+    printf("sendto() failed with error code : %d", WSAGetLastError());
+    exit(EXIT_FAILURE);
+  }
 }
 
 int main(int argc, char **argv) {
@@ -130,54 +174,111 @@ int main(int argc, char **argv) {
   float fx = zed->getParameters()->LeftCam.fx;
   float fy = zed->getParameters()->LeftCam.fy;
 
+#ifndef __linux__
+  WSADATA wsa;
+  //Initialise winsock
+  printf("\nInitialising Winsock...");
+  if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+  {
+    printf("Failed. Error Code : %d", WSAGetLastError());
+    exit(EXIT_FAILURE);
+  }
+  printf("Initialised.\n");
+#endif
+  struct sockaddr_in si_other;
+  int s, slen = sizeof(si_other);
+
+  //create socket
+  if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR) {
+    printf("socket() failed with error code : %d", WSAGetLastError());
+    exit(EXIT_FAILURE);
+  }
+
+  //setup address structure
+  memset((char *)&si_other, 0, sizeof(si_other));
+  si_other.sin_family = AF_INET;
+  si_other.sin_port = htons(4143);
+  si_other.sin_addr.S_un.S_addr = inet_addr("10.41.43.60");
+
+  int total_width = zed->getImageSize().width;
+  int total_height = zed->getImageSize().height;
+
   char key = ' ';
   bool run = true;
   bool calibrate = true;
-  cv::gpu::GpuMat d_hsv(zed->getImageSize().height, zed->getImageSize().width, CV_8UC1);
+  cv::gpu::GpuMat d_hsv(zed->getImageSize().height, zed->getImageSize().width, CV_8UC4);
+  cv::gpu::GpuMat d_thresh(zed->getImageSize().height, zed->getImageSize().width, CV_8UC1);
   while (run) {
 
     // Grab the current images and compute the disparity
     bool res = zed->grab(RAW);
-    cv::Rect left = findRect(zed->retrieveImage_gpu(LEFT), d_hsv);
-    cv::Rect right = findRect(zed->retrieveImage_gpu(RIGHT), d_hsv);
+    try {
+      cv::Rect left = findRect(zed->retrieveImage_gpu(LEFT), d_hsv, d_thresh);
+      cv::Rect right = findRect(zed->retrieveImage_gpu(RIGHT), d_hsv, d_thresh);
+      //Mat left_img = zed->retrieveImage(LEFT);
+      //Mat right_img = zed->retrieveImage(RIGHT);
+      //cv::Mat left_img_cv = slMat2cvMat(left_img);
+      //cv::Mat right_img_cv = slMat2cvMat(right_img);
+      //cv::Rect left = findRect_cpu(left_img_cv);
+      //cv::Rect right = findRect_cpu(right_img_cv);
 
-    float distance_zed = findDepthMaxConfidence(zed, left);
-    float distance_img = calculateDistance(WIDTH_MM, left.width, fx);
-    float distance_img_h = calculateDistance(HEIGHT_MM, left.height, fy);
+      float distance_zed = findDepthMaxConfidence(zed, left);
+      float distance_img = calculateDistance(WIDTH_MM, left.width, fx);
+      float distance_img_h = calculateDistance(HEIGHT_MM, left.height, fy);
 
-    if (calibrate) {
-      cv::Mat left_cv = slMat2cvMat(zed->retrieveImage(LEFT));
-      cv::Point center;
-      center.x = left.x + left.width / 2.;
-      center.y = left.y + left.height / 2.;
-      cv::circle(left_cv, center, 10, cv::Scalar(0, 0, 255, 1), 3);
-      center.y += 50;
-      cv::putText(left_cv, std::to_string(distance_zed), center,
-                  CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(200, 200, 200, 255),
-                  2);
-      center.y -= 15;
-      cv::putText(left_cv, std::to_string(distance_img), center,
-                  CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(200, 200, 200, 255),
-                  2);
-      center.y -= 15;
-      cv::putText(left_cv, std::to_string(distance_img_h), center,
-                  CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(200, 200, 200, 255),
-                  2);
-      cv::imshow("Test", left_cv);
-      key = cv::waitKey(2000);
-
-      switch (key) // handle the pressed key
+      float yaw = (left.x + left.width / 2. - total_width / 2.) / total_width * 2.;
+      float pitch = (total_height / 2. - left.y + left.height / 2.) / total_height * 2.;
+      std::stringstream message;
+      message << std::to_string(distance_img_h) << "," << std::to_string(yaw) << "," << std::to_string(pitch);
+      std::cout << message.str() << std::endl;
+      if (sendto(s, message.str().c_str(), message.str().size(), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
       {
-      case 'q': // close the program
-      case 'Q':
-        run = false;
-        break;
-      default:
-        break;
+        printf("sendto() failed with error code : %d", WSAGetLastError());
+        exit(EXIT_FAILURE);
       }
+
+
+      if (calibrate) {
+        std::cout << std::setprecision(0) << std::fixed << distance_zed << ", " << distance_img << ", " << distance_img_h << std::endl;
+        cv::Mat left_cv = slMat2cvMat(zed->retrieveImage(LEFT));
+        cv::Point center;
+        center.x = left.x + left.width / 2.;
+        center.y = left.y + left.height / 2.;
+        cv::circle(left_cv, center, 10, cv::Scalar(0, 0, 255, 1), 3);
+        center.y += 50;
+        cv::putText(left_cv, std::to_string(distance_zed), center,
+                    CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(200, 200, 200, 255),
+                    2);
+        center.y -= 15;
+        cv::putText(left_cv, std::to_string(distance_img), center,
+                    CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(200, 200, 200, 255),
+                    2);
+        center.y -= 15;
+        cv::putText(left_cv, std::to_string(distance_img_h), center,
+                    CV_FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(200, 200, 200, 255),
+                    2);
+        cv::imshow("Test", left_cv);
+        key = cv::waitKey(2000);
+
+        switch (key) // handle the pressed key
+        {
+        case 'q': // close the program
+        case 'Q':
+          run = false;
+          break;
+        default:
+          break;
+        }
+      }
+    }
+    catch (...) {
     }
   }
 
+  closesocket(s);
+#ifndef __linux__
+  WSACleanup();
+#endif
   delete zed;
   return 0;
 }
